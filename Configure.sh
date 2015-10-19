5cae04b7383037fdf67e60edef89fadce211d183
Petrucio Ricardo <petrucior@gmail.com>
2015-10-19 13:10:37 -0300

    Add files.

---
+++ Configure.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Shell script atualizador dos arquivos asciidoc
+# Author: Petrúcio Ricardo Tavares de Medeiros
+# Date: October, 2015.
+
+echo "Atualizando. . .";
+
+# Gerar index.html para o diretorio de disciplinas
+cd Disciplinas/GraphicsComputer
+asciidoc -b html5 -a icons -a toc2 -a theme=flask index.txt
+
+# Gerar slides no diretório de disciplinas
+asciidoc rayTracing.txt
