1a0ff287a49ff0628e2e2262543960724dfd44e3
petrucior <petrucior@gmail.com>
2018-11-27 11:08:31 -0300

    Adição de radio buttons para extrair features da imagem
    Foram adicionados radio buttons e configurados para extrair features
    durante a modificação da posição do mouse na imagem. Entretanto, a
    posição do último nível ainda está sem extração de features. Sendo
    necessário modificar o código para contornar essa situação.

--- projects/webFovea/fovea.js
+++ projects/webFovea/fovea.js
@@ -9,14 +9,27 @@ let idMedia = document.getElementById('media');
 let idInputMedia = document.getElementById('inputMedia');
 idInputMedia.addEventListener('change', (e) => { idMedia.src = URL.createObjectURL(e.target.files[0]); }, false);
 
-// Fovear a imagem
+// Check radio
+fovea.verifyRadio = function (){
+    var radioGrayscale = document.getElementById('radio0').checked;
+    var radioLaplacian = document.getElementById('radio1').checked;
+    var radioCanny = document.getElementById('radio2').checked;
+    if ( radioGrayscale ) return 0;
+    if ( radioLaplacian ) return 1;
+    if ( radioCanny ) return 2;
+}
+
+// Foveation of the image loaded
 idMedia.addEventListener('load', function(){
-    fovea.foveatedImage("media", "canvasMedia", 50, 50, 4, 0, 0);
+    var radio = fovea.verifyRadio();
+    fovea.foveatedImage("media", "canvasMedia", 50, 50, 4, 0, 0, radio);
 });
 
 // Init OpenCV
 fovea.initOpencv = function onOpenCvReady() {
     document.body.classList.remove("loading");
+    var radio = fovea.verifyRadio();
+    fovea.foveatedImage("media", "canvasMedia", 50, 50, 4, 0, 0, radio);
 }
 
 // Position fovea
@@ -25,7 +38,13 @@ idMedia.addEventListener('mousemove', function(event){
     var x = Math.floor(event.offsetX - canvasRect.width/2);
     var y = Math.floor(event.offsetY - canvasRect.height/2);
     //document.getElementById('output').innerHTML ="( "+ x +" , "+ y +" )";
-    fovea.foveatedImage("media", "canvasMedia", 50, 50, 4, x, y);
+    var radio = fovea.verifyRadio();
+    try{
+	fovea.foveatedImage("media", "canvasMedia", 50, 50, 4, x, y, radio);
+    }
+    catch{
+	console.log("Can't processing in this moment");
+    }
 });
 
 // Fovea Structure
@@ -53,7 +72,7 @@ fovea.mapLevels2Imagey = function( uy, wy, niveis, k, fy, py ){
 }
 
 // MMF
-fovea.mmf = function( img, ux, uy, wx, wy, niveis, k, fx, fy ){
+fovea.mmf = function( img, ux, uy, wx, wy, niveis, k, fx, fy, modeOperation ){
     let dx = Math.floor(this.deltax( ux, wx, niveis, k, fx ));
     let dy = Math.floor(this.deltay( uy, wy, niveis, k, fy ));
     let sx = Math.floor(this.sizex( ux, wx, niveis, k ));
@@ -66,6 +85,22 @@ fovea.mmf = function( img, ux, uy, wx, wy, niveis, k, fx, fy ){
     if ( k < niveis ){
 	cv.resize(img1, img1, new cv.Size(wx, wy));
     }
+    switch ( modeOperation ){
+    case 0:
+	// Grayscale
+	break;
+    case 1:
+	// Laplacian
+	cv.Laplacian(img1, img1, cv.CV_8U, 1, 1, 0, cv.BORDER_DEFAULT);
+	break;
+    case 2:
+	// Canny
+	cv.Canny(img1, img1, 50, 100, 3, false);
+	break;
+    default:
+	alert("Don't do nothing");
+	break;
+    }
     return img1;
 }
 
@@ -74,9 +109,8 @@ fovea.printBorder = function( imgInput,  imgOutput, limiteInicial, limiteFinal){
     for (let j = limiteInicial[0]; j < limiteInicial[2]; j++){ // Cols
 	// North and South
 	for (let i = limiteInicial[1]; i < limiteInicial[3]; i++){ // Rows
-	    if ( i == limiteFinal[1] ) i = limiteFinal[3];
-	    else
-		imgOutput.ucharPtr(i, j)[0] = imgInput.ucharPtr(i - limiteInicial[1], j - limiteInicial[0])[0];
+	    if ( i == limiteFinal[1] ) i = limiteFinal[3] - 1;
+	    else imgOutput.ucharPtr(i, j)[0] = imgInput.ucharPtr(i - limiteInicial[1], j - limiteInicial[0])[0];
 	}
 	
 	// West and East
@@ -89,7 +123,7 @@ fovea.printBorder = function( imgInput,  imgOutput, limiteInicial, limiteFinal){
 }
 
 // Foveated image
-fovea.foveatedImage = function( idInput, idOutput, wx, wy, niveis, fx, fy){
+fovea.foveatedImage = function( idInput, idOutput, wx, wy, niveis, fx, fy, modeOperation){
     let img = cv.imread(idInput);
     var ux = img.cols; var uy = img.rows;
     cv.cvtColor(img, img, cv.COLOR_RGBA2GRAY, 0);
@@ -99,7 +133,7 @@ fovea.foveatedImage = function( idInput, idOutput, wx, wy, niveis, fx, fy){
 	var vectorInit = [];
 	var vectorFinish = [];
 	console.log("level "+k);
-	imgLevel = this.mmf( img, ux, uy, wx, wy, niveis, k, fx, fy );
+	imgLevel = this.mmf( img, ux, uy, wx, wy, niveis, k, fx, fy, modeOperation );
 	var xi = Math.floor(this.mapLevels2Imagex( ux, wx, niveis, k, fx, 0 ));
 	var yi = Math.floor(this.mapLevels2Imagey( uy, wy, niveis, k, fy, 0 ));
 	var xf = Math.floor(this.mapLevels2Imagex( ux, wx, niveis, k, fx, wx ));
@@ -115,7 +149,10 @@ fovea.foveatedImage = function( idInput, idOutput, wx, wy, niveis, fx, fy){
 	    let yf2 = Math.floor(this.mapLevels2Imagey( uy, wy, niveis, k+1, fy, wy ));
 	    vectorFinish = [xi2, yi2, xf2, yf2];
 	    //console.log("xi2: "+xi2+", yi2: "+yi2+", xf2: "+xf2+", yf2: "+yf2);
+	    var start = new Date();
 	    this.printBorder( imgLevel,  imgFoveated, vectorInit, vectorFinish);
+	    var duration = (new Date() - start);
+	    console.log("duracao: "+duration+" ms");
 	}
 	
 	// Deleting vectors
