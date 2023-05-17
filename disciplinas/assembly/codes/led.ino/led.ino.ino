0413b7749559ea73b3ee5fc4466911a20555177f
petrucior <petrucior@gmail.com>
2023-05-17 07:34:32 -0300

    Adicionando arquivos para uma aula de arduino em assembly

---
+++ disciplinas/assembly/codes/led.ino/led.ino.ino
@@ -0,0 +1,16 @@
+//-------------------------
+// C Code for Blinking LED
+//-------------------------
+extern "C"{
+  void start();
+  void led(byte);
+}
+
+void setup(){
+  start();
+}
+
+void loop(){
+  led(1); delay(200);
+  led(0); delay(200);
+}
---
+++ disciplinas/assembly/codes/leds/led.s
@@ -0,0 +1,20 @@
+.global main			;defining a global function
+	.type main, @function 	;defining the type
+
+main:
+	ldi r16,0b00100000	;ldi loads an immediate into registers 16 through 31.
+	ldi r17,0b00000000
+	; The “register” DDRB (Section 14.4 of the manual), which is at address 0x4,
+	; configures the pins of port B are for input (0) or output (1).
+
+	; out loads to the I/O address the value that is stored in the register
+	
+	; Loading 0b00000001 to address 0x4, making pin 5 of Port B used as output,
+	; and the others as input
+	out 0x4,r16		
+
+LOOP:
+	; Register PORTB (address 0x5) defines whether a high or low signal is sent
+	; to the output pins of port B. Let's send logic 1 (5 volts) into PB5.
+	out 0x5,r16
+	jmp LOOP		; Jump to loop label (infinite loop)
